4d01069e7800652fbe84afc866eaaf2e190fd2d6
autodidakto <autodidakto@gmail.com>
2011-05-11 12:23:12 +0200

    Working Module with temporary interface

---
+++ ExtractBookz.rb
@@ -0,0 +1,74 @@
+module ExtractBookz
+	require 'wriggle' # A DSL for Find
+	require 'FileUtils'
+	$delete_tmp = true
+	$delete_originals = false
+
+	def self.process source, target
+		# Sanitize
+		source = File.expand_path(source)
+		target = File.expand_path(target)
+		raise "Source must exist" unless Dir.exists? source
+		raise "Target must exist" unless Dir.exists? target
+
+		# Is this a folder full of book directories or a single book directory?
+		directory_count = 0
+		wriggle source do |source_dir| 
+			source_dir.directories do |dir| 
+				directory_count += 1 # Source counts as a directory
+		end; end
+
+		directory_count > 1 ? extract_folder(source, target) : extract_book(source, target)
+	end
+
+	def self.extract_folder folder, target
+		# Scan folder for book directories
+		wriggle folder do |scenefolder| 
+			scenefolder.directories do |book_dir| 
+				next if book_dir == folder || book_dir == target
+				extract_book book_dir, target
+		end; end 
+	end 
+
+	def self.extract_book book_dir, target
+		# Prep 
+		book_name = clean_name(book_dir)
+		tmp_book_dir = "#{target}/#{book_name}"
+		FileUtils.mkpath tmp_book_dir
+
+		# Go through book directory
+		wriggle book_dir do |book_dir_files| 
+			book_dir_files.extensions %w(zip) do |zip| 
+				`unzip -o #{zip} -d "#{tmp_book_dir}"` 
+		end; end 
+
+		# Unrar, move to target and rename
+		`unrar -o+ -inul e "#{tmp_book_dir}/"*.rar "#{tmp_book_dir}"`
+		`mv -f "#{tmp_book_dir}"/*.pdf "#{target}/#{book_name}.pdf"`
+
+		FileUtils.rm_rf tmp_book_dir if $delete_tmp
+		FileUtils.rm_rf book_dir if $delete_originals
+	end
+
+	def self.clean_name(book_dir)
+		name = book_dir.dup # Not sure why this is necessary
+		name.sub!(/\/$/,'') # remove ending / if exists
+		name.sub!(/^.*\//,'') # remove from start to last / to get directory name only
+		name.sub!(/.\d{4}.*$/,'') # from .YEAR to end
+		name.sub!(/^.*-/,'') # from beginning to -
+		name.gsub!('.',' ') # spaces for .
+		name.gsub!('_',' ') # spaces for _
+		name.strip! # trailing/leading whitespaces
+		if book_dir[/BBL$/] then # BBL releases don't separate publisher with -
+			name.sub! /^\S*\s*/, '' # remove publisher (first word)
+			name.sub! /\s.{3}$/,'' # remove three letter month
+		end
+		return name
+		# Possible features: Keep year, keep publisher, keep edition
+	end 
+end 
+
+# Until I create a real interface for this module...
+if ARGV[0] then
+	ExtractBookz.process ARGV[0], ARGV[1]
+end
---
+++ README.md
@@ -0,0 +1,14 @@
+# ExtractBookz
+Extract PDFs from scene releases and rename them.
+## Requirements: 
+	* unzip and unrar in your path
+	* Ruby (tested on 1.9.2) the wriggle gem (sudo gem install wriggle)
+	* Un*x (test on Mac OS X)
+Should work on Linux. Windows support soon.
+## Usage:
+	ruby ExtractBookz.rb source target
+Source can either be a single directory like: My.Publisher.-.My.Crazy.Book.2011.ebook-DiGiBook, or a folder that contains a bunch of those directories. ExtractBookz cleans up after itself, and won't delete or change  the original directories. Target is where the PDFs will go. Use "" around source and target if there are spaces in the pathname.
+## TODO: 
+	* Linux/Windows testing. Fewer requirements.
+	* Set PDF "title" and "author" attributes.
+	* GUI (Java or native)
